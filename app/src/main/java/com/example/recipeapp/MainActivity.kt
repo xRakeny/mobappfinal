abb1f70ceaf408ccacdfd9b13c4833e7158d43d1
Kowia <100194384+xRakeny@users.noreply.github.com>
2025-02-08 15:24:02 +0400

    Update MainActivity.kt

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -14,23 +14,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Force light mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Initialize Firebase
         FirebaseApp.initializeApp(this)
 
-        // Set up ViewPager and TabLayout
         val viewPager = findViewById<ViewPager2>(R.id.viewPager)
         val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
 
         val adapter = ViewPagerAdapter(this)
         viewPager.adapter = adapter
 
-        // Connect tabs with ViewPager
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = when (position) {
                 0 -> "Food"
